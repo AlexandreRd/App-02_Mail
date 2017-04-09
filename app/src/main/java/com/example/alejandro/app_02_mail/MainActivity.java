15e0da8db228cb0c10b326c89d68e08e492a46f9
Alejandro <aleerdzmtz15@hotmail.com>
2017-04-09 16:36:32 -0500

    Alex - V1.3 Succesful mailing to hotmail

--- app/src/main/java/com/example/alejandro/app_02_mail/MainActivity.java
+++ app/src/main/java/com/example/alejandro/app_02_mail/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         textMessage = msg.getText().toString();
 
         Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.host", "smtp.googlemail.com");
         props.put("mail.smtp.socketFactory.port", "465");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
@@ -68,8 +68,11 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
         pDialog = ProgressDialog.show(context, "", "Sending Mail...", true);
 
-        RetreiveFeedTask task = new RetreiveFeedTask();
-        task.execute();
+        if(session != null) {
+            RetreiveFeedTask task = new RetreiveFeedTask();
+            task.execute();
+        } else
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
     }
 
     private class RetreiveFeedTask extends AsyncTask<String, Void, String> {
@@ -78,10 +81,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         protected String doInBackground(String... params) {
            try {
                Message message = new MimeMessage(session);
-               message.setFrom(new InternetAddress("testfrom354@gmail.com"));
+               message.setFrom(new InternetAddress("aleerdzmtz15@hotmail.com"));
                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
                message.setSubject(subject);
-               message.setContent(textMessage, "text/html; charset-utf-8");
+               message.setContent(textMessage, "text/html; charset=utf-8");
 
                Transport.send(message);
 
